98dae697600bca9c8d91f8b46b44644d68977749
Siliconlad <siliconlad@protonmail.com>
2020-11-19 01:28:52 +0000

    Add basic fusion logic

--- src/ivr_assignment/fusion/fusion.py
+++ src/ivr_assignment/fusion/fusion.py
@@ -1,12 +1,8 @@
 import rospy
 import message_filters
-import cv2 as cv
 
 from rospy import ROSInterruptException
-from cv_bridge import CvBridge, CvBridgeError
-
 from ivr_assignment.msg import JointsStamped
-from geometry_msgs.msg import PointStamped
 
 
 class Fusion:
@@ -15,35 +11,42 @@ class Fusion:
         # Initialize the node
         rospy.init_node('fusion')
 
-        # Class attributes
-
         # Create publishers
         self.joints_pub = rospy.Publisher("/estimation/joints", JointsStamped, queue_size=1)
-        self.sphere_pub = rospy.Publisher("/estimation/sphere", PointStamped, queue_size=1)
 
         # Set up message filtering
         self.image1_joints_sub = message_filters.Subscriber('/estimation/image1/joints', JointsStamped)
         self.image2_joints_sub = message_filters.Subscriber('/estimation/image2/joints', JointsStamped)
 
-        self.image1_targets_sub = message_filters.Subscriber('/estimation/image1/sphere', PointStamped)
-        self.image2_targets_sub = message_filters.Subscriber('/estimation/image2/sphere', PointStamped)
-
-        ts = message_filters.TimeSynchronizer([self.image1_joints_sub, self.image2_joints_sub,
-                                               self.image1_targets_sub, self.image2_targets_sub])
+        ts = message_filters.TimeSynchronizer([self.image1_joints_sub, self.image2_joints_sub])
         ts.registerCallback(self.callback)
 
-    def callback(self, image1_joints, image2_joints, image1_targets, image2_targets):
-        # TODO: Process points
-
+    def callback(self, image1_joints, image2_joints):
         # Publish final estimated joint positions
         joints = JointsStamped()
         joints.header.stamp = rospy.Time.now()
-        self.joints_pub.publish(joints)
 
-        # Publish final estimated sphere position
-        sphere = PointStamped()
-        sphere.header.stamp = rospy.Time.now()
-        self.sphere_pub.publish(sphere)
+        # Yellow joint is the origin
+        joints.joints.yellow.x = 0
+        joints.joints.yellow.y = 0
+        joints.joints.yellow.z = 0
+
+        # Blue joint position
+        joints.joints.blue.x = image2_joints.joints.blue.x
+        joints.joints.blue.y = image1_joints.joints.blue.y
+        joints.joints.blue.z = (image1_joints.joints.blue.z + image2_joints.joints.blue.z) / 2
+
+        # Green joint position
+        joints.joints.green.x = image2_joints.joints.green.x
+        joints.joints.green.y = image1_joints.joints.green.y
+        joints.joints.green.z = (image1_joints.joints.green.z + image2_joints.joints.green.z) / 2
+
+        # Red joint position
+        joints.joints.red.x = image2_joints.joints.red.x
+        joints.joints.red.y = image1_joints.joints.red.y
+        joints.joints.red.z = (image1_joints.joints.red.z + image2_joints.joints.red.z) / 2
+
+        self.joints_pub.publish(joints)
 
 
 def main():
