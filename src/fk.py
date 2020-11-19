f921637c2b27ffea8c3bf4f54de5ad8c6332565d
Austin Pan <austinpan8@gmail.com>
2020-11-19 06:50:11 -0800

    parsed pos and orientation

--- src/fk.py
+++ src/fk.py
@@ -5,7 +5,11 @@ def fk(angles):
                   [cos(z)*(sin(w)*sin(y)-cos(w)*sin(x)*cos(y))-cos(w)*cos(x)*sin(z), -cos(w)*cos(x)*cos(z)-sin(z)*(sin(w)*sin(y)-cos(w)*sin(x)*cos(y)), cos(w)*sin(x)*sin(y)+sin(w)*cos(y), 3.5*(sin(w)*sin(y)-cos(w)*sin(x)*cos(y))+3*(cos(z)*(sin(w)*sin(y)-cos(w)*sin(x)*cos(y))-cos(w)*cos(x)*sin(z))],
                   [cos(x)*cos(y)*cos(z)-sin(x)*sin(z), -cos(x)*cos(y)*sin(z)-sin(x)*cos(z), -cos(x)*sin(y), 3.5*cos(x)*cos(y)+3*(cos(x)*cos(y)*cos(z)-sin(x)*sin(z))+2.5],
                   [0, 0, 0, 1]])
-    return(t)
+    orientation = t[:3, :3]
+    position = t[:3, -1]
+    return position, orientation
 
 a = [0, 0, 0, 0]
-print(fk(a))
+pos, ori = fk(a)
+print(pos)
+print(ori)
