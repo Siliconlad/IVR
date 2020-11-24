07573649f262cc94a0a33b9690d357a4b7270b52
Siliconlad <siliconlad@protonmail.com>
2020-11-24 16:12:46 +0000

    Add forward kinematics node that publishes positions

---
+++ src/ivr_assignment/control/fk.py
@@ -0,0 +1,92 @@
+import math
+import rospy
+import message_filters
+import numpy as np
+from math import sin, cos
+
+from rospy import ROSInterruptException
+from std_msgs.msg import Float64
+from ivr_assignment.msg import PointStamped, FloatStamped
+
+class ForwardKinematics:
+
+    def __init__(self):
+        # Initialize the node
+        rospy.init_node('forward_kinematics')
+
+        # Create publishers
+        self.green_pub = rospy.Publisher("/fk/joint/green", PointStamped, queue_size=1)
+        self.red_pub = rospy.Publisher("/fk/joint/red", PointStamped, queue_size=1)
+
+        # Create subscriber
+        self.joint2_sub = message_filters.Subscriber('/joint2/angle', FloatStamped)
+        self.joint3_sub = message_filters.Subscriber('/joint3/angle', FloatStamped)
+        self.joint4_sub = message_filters.Subscriber('/joint4/angle', FloatStamped)
+
+        ts = message_filters.ApproximateTimeSynchronizer([self.joint2_sub, self.joint3_sub, self.joint4_sub],
+                                                         queue_size=1, slop=0.05)
+        ts.registerCallback(self.callback)
+
+        # Set rate to 10Hz
+        self.rate = rospy.Rate(100)
+
+    def callback(self, joint2, joint3, joint4):
+        theta_1 = 0
+        theta_2 = joint2.angle
+        theta_3 = joint3.angle
+        theta_4 = joint4.angle
+
+        a_0_1 = np.array([[ sin(theta_1),  0, cos(theta_1),   0],
+                          [-cos(theta_1),  0, sin(theta_1),   0],
+                          [            0, -1,            0, 2.5],
+                          [            0,  0,            0,   1]])
+
+        a_1_2 = np.array([[ sin(theta_2), 0, -cos(theta_2), 0],
+                          [-cos(theta_2), 0, -sin(theta_2), 0],
+                          [            0, 1,             0, 0],
+                          [            0,  0,             0, 1]])
+
+        a_2_3 = np.array([[cos(theta_3),  0, -sin(theta_3), 3.5*cos(theta_3)],
+                          [sin(theta_3),  0,  cos(theta_3), 3.5*sin(theta_3)],
+                          [           0, -1,             0,                0],
+                          [           0,  0,             0,                1]])
+
+        a_3_4 = np.array([[ cos(theta_4), -sin(theta_4), 0, 3*cos(theta_4)],
+                          [ sin(theta_4),  cos(theta_4), 0, 3*sin(theta_4)],
+                          [            0,             0, 1,              0],
+                          [            0,             0, 0,              1]])
+
+        ## Multiply
+        a_0_2 = np.matmul(a_0_1, a_1_2)
+        a_0_3 = np.matmul(a_0_2, a_2_3)
+        a_0_4 = np.matmul(a_0_3, a_3_4)
+
+        # Green position
+        g_x, g_y, g_z, _ = a_0_3[:, -1].reshape(-1)
+        msg = PointStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.point.x = g_x
+        msg.point.y = g_y
+        msg.point.z = g_z
+        self.green_pub.publish(msg)
+
+        # Red position
+        r_x, r_y, r_z, _ = a_0_4[:, -1].reshape(-1)
+        msg = PointStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.point.x = r_x
+        msg.point.y = r_y
+        msg.point.z = r_z
+        self.red_pub.publish(msg)
+
+def main():
+    _ = ForwardKinematics()
+
+    try:
+        rospy.spin()
+    except ROSInterruptException:
+        print("Shutting down...")
+
+
+if __name__ == '__main__':
+    main()
--- src/ivr_assignment/control/joint_controller.py
+++ src/ivr_assignment/control/joint_controller.py
@@ -3,6 +3,7 @@ import rospy
 
 from rospy import ROSInterruptException
 from std_msgs.msg import Float64
+from ivr_assignment.msg import FloatStamped
 
 
 class JointController:
@@ -15,6 +16,10 @@ class JointController:
         self.joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=1)
         self.joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=1)
         self.joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=1)
+
+        self.joint2_truth_pub = rospy.Publisher("/joint2/angle", FloatStamped, queue_size=1)
+        self.joint3_truth_pub = rospy.Publisher("/joint3/angle", FloatStamped, queue_size=1)
+        self.joint4_truth_pub = rospy.Publisher("/joint4/angle", FloatStamped, queue_size=1)
 
         # Set rate to 10Hz
         self.rate = rospy.Rate(100)
@@ -31,15 +36,31 @@ class JointController:
             joint2_angle.data = (math.pi/2) * math.sin((math.pi/15) * current_time)
             self.joint2_pub.publish(joint2_angle)
 
+            joint2_angle = FloatStamped()
+            joint2_angle.angle = (math.pi/2) * math.sin((math.pi/15) * current_time)
+            joint2_angle.header.stamp = rospy.Time.now()
+            self.joint2_truth_pub.publish(joint2_angle)
+
             # Publish angle for joint 3
             joint3_angle = Float64()
             joint3_angle.data = (math.pi/2) * math.sin((math.pi/18) * current_time)
             self.joint3_pub.publish(joint3_angle)
+            self.joint3_pub.publish(joint3_angle)
+
+            joint3_angle = FloatStamped()
+            joint3_angle.angle = (math.pi/2) * math.sin((math.pi/18) * current_time)
+            joint3_angle.header.stamp = rospy.Time.now()
+            self.joint3_truth_pub.publish(joint3_angle)
 
             # Publish angle for joint 4
             joint4_angle = Float64()
             joint4_angle.data = (math.pi/3) * math.sin((math.pi/20) * current_time)
             self.joint4_pub.publish(joint4_angle)
+
+            joint4_angle = FloatStamped()
+            joint4_angle.angle = (math.pi/3) * math.sin((math.pi/20) * current_time)
+            joint4_angle.header.stamp = rospy.Time.now()
+            self.joint4_truth_pub.publish(joint4_angle)
 
             # Sleep
             self.rate.sleep()
